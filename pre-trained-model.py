37eb6e963aea6b6fa20c58b22dc839fe9160b6fc
sbkr74 <sbkr74@gmail.com>
2024-07-29 21:31:45 +0530

    pre-trained-model.py file updated(tar.gz extraction)

--- pre-trained-model.py
+++ pre-trained-model.py
@@ -4,10 +4,11 @@ PRETRAINED_MODEL_PATH = os.path.join('TensorFlow','workspace','training_demo','p
 PRETRAINED_MODEL_NAME = "ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8"
 
 print(f"cd {PRETRAINED_MODEL_PATH}")
-print(f"wget.download({PRETRAINED_MODEL_URL})")
+print(f"wget.download('{PRETRAINED_MODEL_URL}')")
 print(f"cd {PRETRAINED_MODEL_PATH} && tar -zxvf {PRETRAINED_MODEL_NAME+'.tar.gz'}")
 
 # In Terminal 
 '''
 python -c "import wget;wget.download('http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz')"
+tar -zxvf ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz
 '''
