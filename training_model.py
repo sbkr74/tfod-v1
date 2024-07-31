99a2a8f7701fa56e00a3a4113379de0eaf6dba3a
sbkr74 <sbkr74@gmail.com>
2024-07-31 21:14:10 +0530

    training_model.py file added

---
+++ training_model.py
@@ -0,0 +1,15 @@
+import os
+
+CUSTOM_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite'
+paths = {
+    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','training_demo','models',CUSTOM_MODEL_NAME)
+}
+files = {
+    'PIPELINE_CONFIG':os.path.join('TensorFlow', 'workspace','training_demo','models', CUSTOM_MODEL_NAME, 'pipeline.config')
+}
+
+TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
+command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'])
+
+print(command)
