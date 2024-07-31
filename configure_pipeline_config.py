63ffe1519577dc4363acc3c1b29aed0e61261b61
sbkr74 <sbkr74@gmail.com>
2024-07-31 11:35:05 +0530

    update2.0 (modification write to file)

--- configure_pipeline_config.py
+++ configure_pipeline_config.py
@@ -17,7 +17,7 @@ files = {
     'PIPELINE_CONFIG':os.path.join('TensorFlow', 'workspace','training_demo','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
     'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
 }
-
+labels = [{'name':'hi', 'id':1}, {'name':'ok', 'id':2}, {'name':'iloveyou', 'id':3}, {'name':'peace', 'id':4}, {'name':'call', 'id':5}]
 config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
 
 # python -c "from object_detection.utils import config_util;config_path=r'TensorFlow\workspace\training_demo\models\ssd_mobnet_v2_fpnlite\pipeline.config';config = config_util.get_configs_from_pipeline_file(config_path);print(config)"
@@ -26,8 +26,7 @@ pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
 with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "r") as f:                                                                                                                                                                                                                     
     proto_str = f.read()                                                                                                                                                                                                                                          
     text_format.Merge(proto_str, pipeline_config)
-labels = [{'name':'hi', 'id':1}, {'name':'ok', 'id':2}, {'name':'iloveyou', 'id':3}, {'name':'peace', 'id':4}, {'name':'call', 'id':5}]
-print(len(labels))
+
 pipeline_config.model.ssd.num_classes = len(labels)
 pipeline_config.train_config.batch_size = 4
 pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
@@ -36,5 +35,9 @@ pipeline_config.train_input_reader.label_map_path= files['LABELMAP']
 pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
 pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
 pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
+
+config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:                                                                                                                                                                                                                     
+    f.write(config_text)  
 
 print("End of script")
